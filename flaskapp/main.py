937c90e68e241967df272d268c52e08d1f10aaac
Pavel <pavelK@yandex.ru>
2021-06-06 22:07:48 +0700

    Pre-Final2

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -3,46 +3,34 @@ print('Hello world')
 from flask import Flask
 from PIL import Image
 app = Flask(__name__)
-
-
 from flask import render_template, request
-SECRET_KEY = 'secret'
-app.config['SECRET_KEY'] = SECRET_KEY
-
-app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = 'PLACE KEY FROM GOOGLE HERE'
-app.config['RECAPTCHA_PRIVATE_KEY'] = 'PLACE SECRET KEY FROM GOOGLE HERE'
-app.config['RECAPTCHA_OPTIONS'] = {'theme':'white'}
-@app.route("/data_to")
-def data_to():
-  some_pars = {'user':'Ivan', 'color':'red'}
-  some_str = 'Hello my dear friends!'
-  some_value = 10
-  
-  return render_template('simple.html', some_str = some_str, some_value = some_value,
-                          some_pars = some_pars)
-
-# @app.route("/", methods = ("GET", "POST"))
-# def hello():
-#   return "<html><title>Some title</title><body><h1>Some text<h1></body></html>"
 
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, TextField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-#from keras.preprocessing import image
 
-from flask_bootstrap import Bootstrap
-bootstrap = Bootstrap(app)
+# from flask_bootstrap import Bootstrap
+# bootstrap = Bootstrap(app)
 
 
-class NetForm(FlaskForm):
-  openid = StringField('openid', validators = [DataRequired()])
-  upload = FileField('Load image', validators = [
-                      FileRequired(),
-                      FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
-  recaptcha = RecaptchaField()
-  submit = SubmitField('send')
+# class NetForm(FlaskForm):
+#  openid = StringField('openid', validators = [DataRequired()])
+#  upload = FileField('Load image', validators = [
+#                      FileRequired(),
+#                      FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+#  recaptcha = RecaptchaField()
+#  submit = SubmitField('send')
+app.config['SECRET_KEY'] = "12345"
+
+#  ключи recaptcha от google
+app.config['RECAPTCHA_PUBLIC_KEY'] = "6Ld74-oUAAAAAJC0UOY6PtrOrNcxQ2VQCfGAqBOC"
+app.config['RECAPTCHA_PRIVATE_KEY'] = "6Ld74-oUAAAAAD2_Jl2IVKh2uCCI9OPX_7oTdLz4"
+app.config['RECAPTCHA_DISABLE'] = True  # будет капча или нет
+
+class ContactForm(FlaskForm):
+    text = TextField('Введите что-нибудь, чтобы доказать, что Вы не робот:', validators=[DataRequired()])
+
 
 
 UPLOAD_FOLDER = 'static/'
@@ -58,10 +46,10 @@ def create_chart(path, root):
     import matplotlib.pyplot as plt
     image = io.imread(path)
 
-    _ = plt.hist(image.ravel(), bins = 64, color = 'orange', )
-    _ = plt.hist(image[:, :, 0].ravel(), bins = 64, color = 'red', alpha = 0.5)
-    _ = plt.hist(image[:, :, 1].ravel(), bins = 64, color = 'Green', alpha = 0.5)
-    _ = plt.hist(image[:, :, 2].ravel(), bins = 64, color = 'Blue', alpha = 0.5)
+    _ = plt.hist(image.ravel(), bins=64, color='orange', )
+    _ = plt.hist(image[:, :, 0].ravel(), bins=64, color='red', alpha = 0.5)
+    _ = plt.hist(image[:, :, 1].ravel(), bins=64, color='Green', alpha = 0.5)
+    _ = plt.hist(image[:, :, 2].ravel(), bins=64, color='Blue', alpha = 0.5)
     _ = plt.xlabel('Intensity Value')
     _ = plt.ylabel('Count')
     _ = plt.legend(['Total', 'Red Channel', 'Green Channel', 'Blue Channel'])
@@ -70,8 +58,18 @@ def create_chart(path, root):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    global form
+    form = ContactForm()
+    global msg
+    msg = ""
     if request.method == 'POST':
         try:
+            if form.validate_on_submit():
+                msg = "Успех!"
+            else:
+                msg = "Ошибка валидации"
+                return render_template('mainPage.html',
+                                       a = 1, b = 1, form = form, msg = msg)
             global file
             file = request.files['file']
             if file and (file.content_type.rsplit('/', 1)[1] in ALLOWED_EXTENSIONS).__bool__():
@@ -79,8 +77,8 @@ def upload_file():
                 filename = secure_filename(file.filename)
                 print(filename)
                 file.save(app.config['UPLOAD_FOLDER'] + filename)
-                return render_template('mainPage.html', picture = app.config['UPLOAD_FOLDER'] + filename, a = 1, b = 1
-                                       )
+                return render_template('mainPage.html', picture = app.config['UPLOAD_FOLDER'] + filename,
+                                       a = 1, b = 1, form = form, msg = msg)
         except Exception:
             print("Зашли в except")
             coeff = request.form.get('rescaleInputField')
@@ -95,18 +93,15 @@ def upload_file():
                 r_picture.save(app.config['UPLOAD_FOLDER'] + 'resized' + filename)
                 create_chart(app.config['UPLOAD_FOLDER'] + filename, app.config['UPLOAD_FOLDER'] + 'graph' + filename)
                 create_chart(app.config['UPLOAD_FOLDER'] + 'resized' + filename, app.config['UPLOAD_FOLDER'] + 'graphresized' + filename)
-                total = ""
-                total += '<p><img src="' + app.config['UPLOAD_FOLDER'] + 'graph' + filename + '" alt="">'
-                total += '<p><img src="' + app.config['UPLOAD_FOLDER'] + 'graphresized' + filename + '" alt="">'
                 return render_template('mainPage.html', picture = app.config['UPLOAD_FOLDER'] + filename,
                                     picture_new = app.config['UPLOAD_FOLDER'] + 'resized' + filename,
                                     a = width, b = height,
                                     chart = app.config['UPLOAD_FOLDER'] + 'graph' + filename,
                                     chart_resized = app.config['UPLOAD_FOLDER'] + 'graphresized' + filename)
 
-            return render_template('mainPage.html', picture = app.config['UPLOAD_FOLDER'] + filename) + "<p>Введено неверное значение!"
+            return render_template('mainPage.html', picture = app.config['UPLOAD_FOLDER'] + filename, form = form, msg = msg) + "<p>Введено неверное значение!"
 
-    return render_template('mainPage.html', picture = "", a = 1, b = 1)
+    return render_template('mainPage.html', picture = "", a = 1, b = 1, form = form, msg = msg)
 
 
 # @app.route('/<path:filename>')
